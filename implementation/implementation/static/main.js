c1997d153c2b06756ba2ec5e80238b4016a9f3e8
Nikolay Zinov <nzinov@gmail.com>
2017-12-09 14:00:08 +0300

    finish frontend

--- implementation/implementation/static/main.js
+++ implementation/implementation/static/main.js
@@ -16,16 +16,19 @@ class View {
         document.getElementById("container").innerHTML = Mustache.render(this.template, this.context);
     }
 
-    request(endpoint, method, context, key) {
+    request(endpoint, method, context, callback) {
         context["method"] = method;
         var xhttp = new XMLHttpRequest();
         var that = this;
-        xhttp.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                that.context[key] = JSON.parse(this.responseText);
-                that.render();
-            }
-        };
+        if (typeof callback === "string") {
+            callback = function() {
+                if (this.readyState == 4 && this.status == 200) {
+                    that.context[callback] = JSON.parse(this.responseText);
+                    that.render();
+                }
+            };
+        }
+        xhttp.onreadystatechange = callback;
         xhttp.open("POST", "/api/"+endpoint+"/", true);
         xhttp.setRequestHeader("Content-type", "application/json");
         xhttp.send(JSON.stringify(context));
@@ -55,6 +58,8 @@ class MainView extends View {
     }
 }
 
+const button = "<button onclick='view.chooseRoute({{id}})'>Выбрать маршрут</button>";
+
 class OrderListView extends View {
     constructor() {
         super()
@@ -62,16 +67,67 @@ class OrderListView extends View {
         "<li>{{from}} -> {{to}} ({{num}} товаров)"+
         "{{#route}}{{#element}}{{#vertex}}{{name}}{{/vertex}}"+
         "{{^vertex}} --{{name}}-> {{/vertex}}{{/element}}{{/route}}"+
-        "{{^route}}<button onclick='view.chooseRoute({{id}})'>Выбрать маршрут</button>{{/route}}</li>"+
+        "{{^route}}<div id='{{id}}'>"+button+"</div>{{/route}}</li>"+
         "{{/orders}}</ol>{{^orders}}Загрузка{{/orders}}"
+        this.load()
+    }
+    
+    load() {
         this.request("order_list", "get_orders", {}, "orders")
     }
+
+    chooseRoute(order) {
+        var that = this;
+        this.request("order_list", "get_routes", {"order": order}, function() {
+            var context = JSON.parse(this.responseText);
+            document.getElementById(order).innerHTML = Mustache.render("<select onchange='view.onselect({{order}}, this)'>{{#routes}}<option value={{index}}>{{length}} дн. {{cost}} руб. {{type}}{{/routes}}", context)
+        });
+    }
+
+    onselect(order, elem) {
+        this.selectRoute(order, this.val);
+    }
+
+    selectRoute(order, index) {
+        var that = this;
+        this.request("order_list", "select_route", {"order": order, "index": index}, function () {
+            that.load();
+        });
+    }
 }
+
+const product = "<input name={{index}}>кг.<br/>";
 
 class PlaceOrderView extends View {
     constructor() {
         super()
-        this.template = "no";
+        this.template = "Введите параметры заказа:<br/><form name='main'>"+
+            "Откуда: <input name='from'><br/>"+
+            "Куда: <input name='to'><br/>"+
+            "Товары: <button onclick='view.addProduct(); return false;'>+</button><br/>"+
+            "<div id='products'>"+Mustache.render(product, {"index": 0})+"</div>"+
+            "<button onclick='view.submit()'>Добавить заказ</button></form>";
+        this.product_num = 1;
+    }
+
+    addProduct() {
+        document.getElementById("products").innerHTML += Mustache.render(product, {"index": this.product_num});
+        this.product_num += 1;
+    }
+
+    submit() {
+        var form = document.forms["main"];
+        var products = [];
+        for (var i = 0; i < this.product_num; ++i) {
+            products[i] = form[i.toString()].value;
+        }
+        var data = {
+            "from": form["from"].value,
+            "to": form["to"].value,
+            "products": products,
+        }
+        this.request("order_list", "place_order", data, init);
+        return false;
     }
 }
 
--- implementation/implementation/views.py
+++ implementation/implementation/views.py
@@ -1,16 +1,21 @@
 from django.views.generic import View
 from django.http import JsonResponse
+import json
 
 
 class APIEndpoint(View):
     def post(self, request):
-        result = getattr(self, request.POST["method"])(**request.POST)
-        return JsonResponse(result)
+        data = json.loads(request.body)
+        result = getattr(self, data["method"])(**data)
+        return JsonResponse(result, safe=False)
 
 
 class OrderListEndpoint(APIEndpoint):
-    def get_orders(self, order, **kwargs):
-        pass
+    def get_orders(self, **kwargs):
+        return []
 
-    def select_route(self, order, index):
-        pass
+    def get_routes(self, order, **kwargs):
+        return {}
+
+    def select_route(self, order, index, **kwargs):
+        return {}
